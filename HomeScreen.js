4152c7086dc1b10102a01384648e1dfb10a5e815
HarshVajani <70755020+HarshVajani@users.noreply.github.com>
2022-01-07 16:36:11 +0530

    Add files via upload

---
+++ HomeScreen.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react"; 
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+import { Header, AirbnbRating, Icon } from "react-native-elements"; 
+import { RFValue } from "react-native-responsive-fontsize";
+import axios from "axios";
+
+
